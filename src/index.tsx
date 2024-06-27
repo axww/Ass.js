9c8bb10736b4671b18d09f0d932a6c9602e074b2
awwr <114227169+awwr@users.noreply.github.com>
2024-06-27 18:11:37 +0900

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,23 @@ const app = new Hono<{
   }
 }>()
 
+// 在应用启动时调用创建表的函数
+app.on('fetch', '', async (c) => {
+  const tableCreationQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE
+  );
+`
+  try {
+    await c.env.DB.prepare(tableCreationQuery).all();
+    console.log('Table created successfully.')
+  } catch (error) {
+    console.error('Error creating table:', error)
+  }
+})
+
 app.use(renderer)
 
 app.get('/', (c) => {
@@ -19,11 +36,10 @@ app.get("/user/:id", async (c) => {
   try {
     let { results } = await c.env.DB.prepare(
       "SELECT * FROM users WHERE user_id = ?",
-    )
-      .bind(userId)
-      .all();
+    ).bind(userId).all();
     return c.json(results);
   } catch (e) {
+    console.log(Object.keys(e))
     return c.json({ err: e.message }, 500);
   }
 });
